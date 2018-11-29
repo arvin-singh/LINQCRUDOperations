66922e5c1a1c7da63964c3d43ef5cf2764f71c5e
Arvind Singh <arvin.techedge@gmail.com>
2018-11-29 18:17:53 +0530

    Added LINQCRUD Operations Source Code

---
+++ LINQCRUDOperations/LINQCRUDHINDI/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LINQCRUDHINDI.Models;
+
+namespace LINQCRUDHINDI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DataClasses1DataContext obj = new DataClasses1DataContext();
+        public ActionResult Index()
+        {
+            var data = obj.employees.ToList();
+            return View(data);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            var databyid = obj.employees.Single(x => x.empid==id);
+            return View(databyid);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
